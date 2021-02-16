37f220be1def2a6f314fff423dd5428fc05bd934
Eren Inanci <erencan_inanci@hotmail.com>
2021-02-16 21:46:34 +0300

    Create SaveLevel.cs

---
+++ SaveLevel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public void Save()
+    {
+        PlayerPrefs.SetInt("Level4", SceneManager.GetActiveScene().buildIndex);
+        PlayerPrefs.Save();
+        Debug.Log("SavedLevel: "+PlayerPrefs.GetInt("Level4"));
+    }
+
+    public void Load()
+    {
+        if(PlayerPrefs.GetInt("Level4") > 0)
+        {
+            if(PlayerPrefs.GetInt("Level4") < SceneManager.GetActiveScene().buildIndex)
+            {
+
+            }
+            else if(PlayerPrefs.GetInt("Level4") == SceneManager.GetActiveScene().buildIndex)
+            {
+                
+            }
+            else
+            {
+                SceneManager.LoadScene(PlayerPrefs.GetInt("Level4"));
+            }
+        }
+    }
+
+    void Start()
+    {
+        Load();
+    }
+
+    void Update()
+    {
+        Save();
+    }
+}
